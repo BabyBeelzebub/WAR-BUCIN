b9d59382e7b16e740541fdbf05ff6f069702bfa0
BabyBeelzebub <89534912+BabyBeelzebub@users.noreply.github.com>
2021-09-06 00:08:28 +0700

    Update beeinvite.py

--- userbot/modules/beeinvite.py
+++ userbot/modules/beeinvite.py
@@ -50,7 +50,58 @@ async def get_chatinfo(event):
             return None
     return chat_info
 
+@register(outgoing=True, pattern=r"^\.getmemb$")
+async def scrapmem(event):
+    chat = event.chat_id
+    await event.edit("`Mohon tunggu...`")
+    event.client
+    members = await event.client.get_participants(chat, aggressive=True)
 
+    with open("members.csv", "w", encoding="UTF-8") as f:
+        writer = csv.writer(f, delimiter=",", lineterminator="\n")
+        writer.writerow(["user_id", "hash"])
+        for member in members:
+            writer.writerow([member.id, member.access_hash])
+    await event.edit("`Berhasil Mengumpulkan Member..`")
+
+
+@register(outgoing=True, pattern=r"^\.addmemb$")
+async def admem(event):
+    await event.edit("`Proses Menambahkan 0 Member...`")
+    chat = await event.get_chat()
+    event.client
+    users = []
+    with open("members.csv", encoding="UTF-8") as f:
+        rows = csv.reader(f, delimiter=",", lineterminator="\n")
+        next(rows, None)
+        for row in rows:
+            user = {'id': int(row[0]), 'hash': int(row[1])}
+            users.append(user)
+    n = 0
+    for user in users:
+        n += 1
+        if n % 30 == 0:
+            await event.edit(f"`Mencapai 30 anggota, tunggu selama {900/60} menit`")
+            await asyncio.sleep(900)
+        try:
+            userin = InputPeerUser(user['id'], user['hash'])
+            await event.client(InviteToChannelRequest(chat, [userin]))
+            await asyncio.sleep(random.randrange(5, 7))
+            await event.edit(f"`Prosess Menambahkan {n} Member...`")
+        except TypeError:
+            n -= 1
+            continue
+        except UserAlreadyParticipantError:
+            n -= 1
+            continue
+        except UserPrivacyRestrictedError:
+            n -= 1
+            continue
+        except UserNotMutualContactError:
+            n -= 1
+            continue
+            
+            
 @register(outgoing=True, pattern=r"^\.inviteall(?: |$)(.*)")
 async def get_users(event):
     sender = await event.get_sender()
