9667ceef393c506269b4f1b56f4ff955382933a5
•ᴄʜєᴧ✞̸.ɛxɛ✰̶✰̶ <89534912+BabyBeelzebub@users.noreply.github.com>
2021-09-18 05:40:50 +0700

    Update animasi6.py

--- userbot/modules/animasi6.py
+++ userbot/modules/animasi6.py
@@ -157,7 +157,7 @@ async def typewriter(typew):
     await typew.edit("`I LOVE YOU 💞`")
 
 
-@register(outgoing=True, pattern='^.lopul(?: |$)(.*)')
+@register(outgoing=True, pattern='^.lopulin(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
